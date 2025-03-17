9c058a4182f636a8d045595481bc8d2ace8847b4
yuta4431 <orange_1272@yahoo.co.jp>
2025-03-17 21:50:33 +0900

    パーソナリティ名変更

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,5 +1,5 @@
 class Personality < ApplicationRecord
-  has_many :program_personalities
+  has_many :program_personalities, dependent: :destroy
   has_many :programs, through: :program_personalities
 
   validates :name, presence: true, length: { maximum: 255 }
